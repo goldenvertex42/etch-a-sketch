b8e38c0d42bc44b2755dc2b0d80d0cd152c787f9
goldenvertex42 <fabled93@gmail.com>
2025-08-14 17:43:40 -0500

    Add comments and make grid size and total cells variables mutable

--- javascript.js
+++ javascript.js
@@ -1,15 +1,15 @@
+// Define the grid container
 const gridContainer = document.getElementById("grid-container");
-const gridSize = 16;
-const totalCells = gridSize * gridSize;
-
+// Set variables for grid size and total cells to be used for changing the grid later
+let gridSize = 16;
+let totalCells = gridSize * gridSize;
+// For loop that creates a grid
 for (let i = 0; i < totalCells; i++) {
     const cell = document.createElement('div');
     cell.classList.add('grid-cell');
     gridContainer.appendChild(cell);
 }
-
-
-
+// Event listener that executes a background color change on the element if it has 'grid-cell' class 
 gridContainer.addEventListener('mouseover', function(e) {
     if (e.target.classList.contains('grid-cell')) {
         e.target.style.backgroundColor = 'black';
